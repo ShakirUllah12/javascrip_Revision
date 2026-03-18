d59cbd54125ad82919c06a90cd322b7f89b27b90
ShakirUllah12 <shakirkhangadooni@gmail.com>
2026-03-18 23:36:32 +0500

    class 6 is done

--- class_5/script.js
+++ class_5/script.js
@@ -3,3 +3,77 @@
 // let cond1= a > b;
 // let cond2=a===6;
 // console.log(cond1 && cond2);
+// let a=6;
+// let b=5;
+// console.log(a < b || a===6);
+// console.log(!(a<b))
+// let age=15;
+// if(age >=18){
+//     console.log("you are eligable")
+// }
+// if(age <18){
+//     console.log("you are not eligable")
+// }
+// let mode="dark";
+// let color;
+// if (mode === "dark"){
+//     color="black"
+// }
+// console.log(color)
+// let mode="light";
+// let color;
+// if(mode === "light"){
+//     color="red"
+// }
+
+// console.log(color)
+// let mode ="dark";
+// let color;
+// if(mode ==="light"){
+//     color="red"
+// }
+// else{
+//     color="white"
+// }
+// console.log(color);
+// let age=34;
+// if(age >20){
+//     console.log("you can")
+// }
+// else{
+//     console.log("you cant")
+// }
+// console.log(age);
+
+// let num=9;
+// if(num%2 ===0){
+//     console.log("even Number")
+// }
+// else {
+//     console.log("odd Number")
+// }
+// let  mode="purple";
+// let color;
+// if(mode ==="dark"){
+//     color="red"
+// }
+// else if(mode==="light"){
+//     color="light"
+// }
+// else if(mode ==="gray"){
+//     color="gray"
+// }
+// else if(mode === "purple"){
+//     color="purple"
+// }
+// console.log(color);
+// let age=25;
+// let result=age>18?"adult":"junior";
+// console.log(result);
+let num=prompt("enter a number");
+if(num%5 === 0){
+    console.log("yes 5 multipe")
+}
+else{
+    console.log("not equal to 5")
+}
---
+++ class_6/script.js
@@ -0,0 +1,25 @@
+// let i=10;
+// for(let i=0; i<=10; i++){
+//     console.log("My name is shakir ullah")
+// }
+// let sum=0;
+// for(let i =1; i <=5; i++){
+//     sum=sum+i;
+// }
+// console.log("my name is shakir ullah",sum)
+// while loop in javascrip 
+// let i=0;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+// let i=0;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+let i=10;
+do{
+    console.log("My name is shakir ullah")
+}while(i<=5);
